51d4f1dd9ddcb892e8496806f2ba5a9fbd94d030
Petr Timofeev <p.timofeev@alt1.ru>
2017-02-16 18:23:23 +0300

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "alba-client-python",
-    version = "1.0.4",
+    version = "1.0.5",
     url = 'https://github.com/RFIBANK/alba-client-python',
     license = 'GPL v3',
     description = "API client for Alba.",
